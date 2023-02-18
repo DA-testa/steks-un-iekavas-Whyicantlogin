f927bc7a41d2c4a507eadf8abd4b34adb557484f
Whyicantlogin <114979508+Whyicantlogin@users.noreply.github.com>
2023-02-18 20:11:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,18 +11,27 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
+    mismatch_pos = None
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(next)
+            if len(opening_brackets_stack)<2:
+                mismatch_pos = Bracket(next, i+1).position
+            
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
-
+            if len(opening_brackets_stack) == 0:
+                return Bracket(next, i+1).position
+        if len(opening_brackets_stack) == 0:
+            retrun "Success"
+        return mismatch_pos
 
 def main():
     text = input()
+    if text[0] == "I":
+        text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
 
