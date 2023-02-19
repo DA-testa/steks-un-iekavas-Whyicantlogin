1ff49c6a23d65b29d5c4d4b4b29efddd86207b12
Whyicantlogin <114979508+Whyicantlogin@users.noreply.github.com>
2023-02-19 18:18:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,20 +16,20 @@ def find_mismatch(text):
         if next in "([{":
             # Process opening bracket, write your code here
             #jāpievieno struktūras
-            opening_brackets_stack.append(Bracket(next,i+1))
+            opening_brackets_stack.append(Bracket(next, i + 1))
             
 
         if next in ")]}":
             # Process closing bracket, write your code here
             #empty != (empty nebūs)
             #Matchc are matching
-            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+            if not opening_brackets_stack:  
+                return i+1
+            if not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
             opening_brackets_stack.pop()
 
-        if opening_brackets_stack:
-            return opening_brackets_stack[0].position
-        return "Success"
+        
             
         
 
@@ -38,15 +38,16 @@ def main():
     text = input()
     if text[0] == "I":
         text = input()
-    mismatch = find_mismatch(text)
+        mismatch = find_mismatch(text)
     # Printing answer, write your code here
-    if mismatch:
-        print(mismatch)
-    else:
-        print("Success")
+        if mismatch:
+            print(mismatch)
+        else:
+            print("Success")
 
 
 
 if __name__ == "__main__":
     input("I")
     main()
+
