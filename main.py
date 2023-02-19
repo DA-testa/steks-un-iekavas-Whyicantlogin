b0778ad8b56dbf98c5539af547d8c928b5a542c1
Whyicantlogin <114979508+Whyicantlogin@users.noreply.github.com>
2023-02-19 18:25:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,33 +11,40 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
+    mismatch_pos = None
     for i, next in enumerate(text):
         # enumerate nodod gan kārtas numur, gan simbolu
         if next in "([{":
             # Process opening bracket, write your code here
             #jāpievieno struktūras
-            opening_brackets_stack.append(Bracket(next, i + 1))
+            
+
+            opening_brackets_stack.append(Bracket(next,i+1))
             
 
         if next in ")]}":
             # Process closing bracket, write your code here
             #empty != (empty nebūs)
             #Matchc are matching
-            if not opening_brackets_stack:  
-                return i+1
-            if not are_matching(opening_brackets_stack[-1].char, next):
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
             opening_brackets_stack.pop()
-
+        if opening_brackets_stack:
+            return opening_brackets_stack[0].position
+        return "Success"
+            
         
+
 
 def main():
     text = input()
     if text[0] == "I":
         text = input()
-        mismatch = find_mismatch(text)
+    mismatch = find_mismatch(text)
     # Printing answer, write your code here
-        if mismatch:
-            print(mismatch)
-        else:
-            print("Success")
+    print(mismatch)
+
+
+if __name__ == "__main__":
+    input("I")
+    main()
